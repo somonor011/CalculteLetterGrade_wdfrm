f8da29afc4eaa5d006aa5af283a2cb3b696c7000
somonor011 <somonorhong011@gmail.com>
2024-11-01 00:29:24 +0700

    Design color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,24 +39,26 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(62, 59);
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(41, 63);
             label1.Name = "label1";
-            label1.Size = new Size(122, 20);
+            label1.Size = new Size(142, 23);
             label1.TabIndex = 0;
             label1.Text = "Numberic Grade";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(62, 128);
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(41, 127);
             label2.Name = "label2";
-            label2.Size = new Size(93, 20);
+            label2.Size = new Size(111, 23);
             label2.TabIndex = 1;
             label2.Text = "Letter Grade";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(207, 59);
+            textBox1.Location = new Point(193, 59);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(131, 27);
             textBox1.TabIndex = 2;
@@ -64,27 +66,30 @@
             // label3
             // 
             label3.BorderStyle = BorderStyle.Fixed3D;
-            label3.Location = new Point(207, 128);
+            label3.Location = new Point(193, 125);
             label3.Name = "label3";
             label3.Size = new Size(131, 25);
             label3.TabIndex = 3;
             // 
             // button1
             // 
-            button1.BackColor = Color.Silver;
+            button1.BackColor = Color.DarkGreen;
             button1.Cursor = Cursors.Hand;
-            button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(62, 209);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(41, 210);
             button1.Name = "button1";
-            button1.Size = new Size(168, 30);
+            button1.Size = new Size(192, 30);
             button1.TabIndex = 4;
             button1.Text = "Calculate Letter Grade";
             button1.UseVisualStyleBackColor = false;
             // 
             // exit
             // 
-            exit.BackColor = Color.DarkGray;
-            exit.ForeColor = SystemColors.ActiveCaptionText;
+            exit.BackColor = Color.Red;
+            exit.Cursor = Cursors.Hand;
+            exit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            exit.ForeColor = SystemColors.ButtonHighlight;
             exit.Location = new Point(248, 210);
             exit.Name = "exit";
             exit.Size = new Size(90, 29);
@@ -97,8 +102,8 @@
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(224, 224, 224);
-            ClientSize = new Size(389, 297);
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(389, 274);
             Controls.Add(exit);
             Controls.Add(button1);
             Controls.Add(label3);
