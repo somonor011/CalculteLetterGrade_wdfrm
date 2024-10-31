1f7a049f4c29dace063dd112711377d859859d13
somonor011 <somonorhong011@gmail.com>
2024-10-31 20:11:40 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CalculteLetterGrade_wdfrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            exit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Numberic Grade";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Letter Grade";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(207, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(131, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.Location = new Point(207, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 25);
+            label3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Silver;
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(62, 209);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 30);
+            button1.TabIndex = 4;
+            button1.Text = "Calculate Letter Grade";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // exit
+            // 
+            exit.BackColor = Color.DarkGray;
+            exit.ForeColor = SystemColors.ActiveCaptionText;
+            exit.Location = new Point(248, 210);
+            exit.Name = "exit";
+            exit.Size = new Size(90, 29);
+            exit.TabIndex = 5;
+            exit.Text = "Exit";
+            exit.UseVisualStyleBackColor = false;
+            exit.Click += exit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(389, 297);
+            Controls.Add(exit);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "Calculate Letter Grade";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Button button1;
+        private Button exit;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace CalculteLetterGrade_wdfrm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
